6a2239df1ab1699ed83db74d09d402069743ed51
igor <blackstanislav@gmail.com>
2023-02-23 16:04:46 +0500

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ public class Main {
         System.out.println("Hello!");
 
         int initialAccount = 500;
-        int depositAmount = 1000;
+        int depositAmount = 1001;
         int bonus = depositAmount / 100;
         int finalAccount = initialAccount + depositAmount;
+        int finalAccountBonus = finalAccount + bonus;
 
         if (depositAmount > 1000) {
-            System.out.println("Итоговый счёт: " + finalAccount);
+            System.out.println("Итоговый счёт: " + finalAccountBonus);
             System.out.println("Сумма бонуса: " + bonus);
         } else {
             System.out.println("Итоговый счет: " + finalAccount);
